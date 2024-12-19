9a0666074148e29b3bc4fb41ddeaf35bc10549a0
MengChen <tatsukimengchen@163.com>
2024-12-19 16:07:41 +0800

    C语言程序设计 - 实验十

---
+++ header/menu.h
@@ -0,0 +1,12 @@
+// menu.h
+#include <stdio.h>
+#include <stdlib.h>
+#ifndef MENU_H
+#define MENU_H
+
+void Menu();
+void showMenu();
+int getChoice();
+void route(int choice);
+
+#endif // MENU_H
---
+++ source/prac10.c
@@ -0,0 +1,10 @@
+// prac07.c
+#include "menu.c"
+
+int main()
+{
+  while (1)
+  {
+    Menu();
+  }
+}
